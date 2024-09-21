615cab7b061e5dd0a971a51e1259655f361da101
peerless_hero <peerless_hero@outlook.com>
2024-09-22 00:43:55 +0800

    feat: md文件对本地图片的解析

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,18 +1,29 @@
-import { defineCollection, z } from "astro:content";
+/*
+ * @Author: peerless_hero peerless_hero@outlook.com
+ * @Date: 2024-09-07 03:45:19
+ * @LastEditors: peerless_hero peerless_hero@outlook.com
+ * @LastEditTime: 2024-09-22 00:21:46
+ * @FilePath: \peerless-lessons-learned\src\content\config.ts
+ * @Description:
+ *
+ */
+import { defineCollection, z } from 'astro:content'
+
+const globs = import.meta.glob<string>('../assets/*', { eager: true, import: 'default' })
 
 const posts = defineCollection({
-  type: "content",
+  type: 'content',
   schema: z.object({
     title: z.string(),
     pubDate: z.date(),
     description: z.string(),
     lastModified: z.string().optional(),
-    cover: z.string(),
+    cover: z.string().transform(arg => globs[arg] || arg),
     coverAlt: z.string(),
     category: z.array(z.string()),
     tags: z.array(z.string()),
     author: z.string(),
   }),
-});
+})
 
-export const collections = { posts };
+export const collections = { posts }
