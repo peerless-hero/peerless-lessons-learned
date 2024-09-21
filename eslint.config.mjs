b61913b4d16a5238400101cd5eb4d69bc7573f4e
peerless_hero <peerless_hero@outlook.com>
2024-09-22 00:42:09 +0800

    feat: eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+/*
+ * @Author: peerless_hero peerless_hero@outlook.com
+ * @Date: 2024-09-22 00:34:30
+ * @LastEditors: peerless_hero peerless_hero@outlook.com
+ * @LastEditTime: 2024-09-22 00:34:38
+ * @FilePath: \peerless-lessons-learned\eslint.config.mjs
+ * @Description: eslint配置文件
+ *
+ */
+import antfu from '@antfu/eslint-config'
+
+export default antfu()
