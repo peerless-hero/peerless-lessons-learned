5195d392d29dc1d62324039493daeac84f0c5a81
peerless_hero <peerless_hero@outlook.com>
2025-03-03 00:40:30 +0800

    feat: 更新 ESLint 配置，禁用 JSX 标签间距规则

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,7 @@
  * @Author: peerless_hero peerless_hero@outlook.com
  * @Date: 2024-09-22 00:34:30
  * @LastEditors: peerless_hero peerless_hero@outlook.com
- * @LastEditTime: 2024-09-22 00:34:38
+ * @LastEditTime: 2025-03-03 00:40:22
  * @FilePath: \peerless-lessons-learned\eslint.config.mjs
  * @Description: eslint配置文件
  *
@@ -11,4 +11,8 @@ import antfu from '@antfu/eslint-config'
 
 export default antfu({
   astro: true,
+}, {
+  rules: {
+    'style/jsx-tag-spacing': 'off',
+  },
 })
